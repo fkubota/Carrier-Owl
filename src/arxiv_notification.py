f4c443df7d82ead9e0d93543bcb8986fdc2a05ca
fkubota <rkkubotay@gmail.com>
2019-09-09 11:13:22 +0900

    update

---
+++ src/arxiv_notification.py
@@ -0,0 +1,21 @@
+import ast
+import datetime
+import numpy as np
+import textwrap
+from bs4 import BeautifulSoup
+import requests
+from fastprogress import progress_bar
+from googletrans import Translator
+import slackweb
+
+
+
+
+
+
+
+def main():
+    print("Hello")
+
+if __name__ == "__main__":
+    main()
