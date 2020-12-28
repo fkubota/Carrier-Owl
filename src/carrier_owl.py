a2091fff1561f09dd35480a02b2d3f355176771a
wakamezake <hotwater1367@gmail.com>
2020-12-28 22:28:39 +0900

    Delete unused functions

--- src/carrier_owl.py
+++ src/carrier_owl.py
@@ -28,34 +28,6 @@ class Result:
     score: float = 0.0
 
 
-def get_articles_info(subject):
-    weekday_dict = {0: 'Mon', 1: 'Tue', 2: 'Wed', 3: 'Thu',
-                  4: 'Fri', 5: 'Sat', 6: 'Sun'}
-    url = f'https://arxiv.org/list/{subject}/pastweek?show=100000'
-    response = requests.get(url)
-    html = response.text
-    year = datetime.date.today().year
-
-    # いつの論文データを取得するか
-    bs = BeautifulSoup(html)
-    h3 = bs.find_all('h3')
-    wd = weekday_dict[datetime.datetime.today().weekday()]
-    day = datetime.datetime.today().day
-    today = f'{wd}, {day}'
-
-    # 今日、新しい論文が出てるかどうか(土日とか休みみたい)
-    if today in h3[0].text:
-        idx = 2
-    else:
-        idx = 1
-    articles_html = html.split(f'{year}</h3>')[idx]   # <--------- 要注意
-
-    # 論文それぞれのurlを取得
-    bs = BeautifulSoup(articles_html)
-    id_list = bs.find_all(class_='list-identifier')
-    return id_list
-
-
 def calc_score(abst: str, keywords: dict) -> (float, list):
     sum_score = 0.0
     hit_kwd_list = []
@@ -85,52 +57,6 @@ def search_keyword(_get_article_func, keywords: dict) -> list:
             result = Result(url=url, title=title_trans, abstract=abstract_trans,
                             score=score, words=hit_keywords)
             results.append(result)
-    return results
-
-
-def serch_keywords(id_list, keywords_dict):
-    urls = []
-    titles = []
-    abstracts = []
-    words = []
-    scores = []
-    for id_ in progress_bar(id_list):
-        a = id_.find('a')
-        _url = a.get('href')
-        url = 'https://arxiv.org'+_url
-
-        response = requests.get(url)
-        html = response.text
-
-        bs = BeautifulSoup(html)
-        title = bs.find('meta', attrs={'property': 'og:title'})['content']
-        abstract = bs.find(
-                'meta',
-                attrs={'property': 'og:description'})['content']
-
-        sum_score = 0
-        hit_kwd_list = []
-
-        for word in keywords_dict.keys():
-            score = keywords_dict[word]
-            if word.lower() in abstract.lower():  # 全部小文字にすれば、大文字少文字区別しなくていい
-                sum_score += score
-                hit_kwd_list.append(word)
-        if sum_score != 0:
-            title_trans = get_translated_text('ja', 'en', title)
-            abstract = abstract.replace('\n', '')
-            abstract_trans = get_translated_text('ja', 'en', abstract)
-            abstract_trans = textwrap.wrap(abstract_trans, 40)  # 40行で改行
-            abstract_trans = '\n'.join(abstract_trans)
-
-            urls.append(url)
-            titles.append(title_trans)
-            abstracts.append(abstract_trans)
-            words.append(hit_kwd_list)
-            scores.append(sum_score)
-
-    results = [urls, titles, abstracts, words, scores]
-
     return results
 
 
