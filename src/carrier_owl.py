4ad7f85e0a0e32f92e8f3b21802fa701fac69dc8
fkubota <rkkubotay@gmail.com>
2022-05-07 17:58:52 +0900

    [update]

--- src/carrier_owl.py
+++ src/carrier_owl.py
@@ -126,15 +126,15 @@ def get_translated_text(from_lang: str, to_lang: str, from_text: str) -> str:
     driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
     driver.get(url)
     driver.implicitly_wait(10)  # 見つからないときは、10秒まで待つ
+    print('xxxxx')
+    print(driver)
+    print(1/0)
 
     for i in range(30):
         # 指定時間待つ
         time.sleep(sleep_time)
         html = driver.page_source
 
-        print('xxxxx')
-        print(html)
-        print(1/0)
         to_text = get_text_from_page_source(html)
 
         if to_text:
