bc9bc55474cca8c15b1d131668234cf6c90150dc
fkubota <rkkubotay@gmail.com>
2022-05-08 21:42:47 +0900

    [update] actionsの動作チェックのためにdays=3に一時的に変更していたのをdays=2にもどう #73

--- src/carrier_owl.py
+++ src/carrier_owl.py
@@ -168,8 +168,7 @@ def main():
     keywords = config['keywords']
     score_threshold = float(config['score_threshold'])
 
-    # day_before_yesterday = datetime.datetime.today() - datetime.timedelta(days=2)
-    day_before_yesterday = datetime.datetime.today() - datetime.timedelta(days=3)
+    day_before_yesterday = datetime.datetime.today() - datetime.timedelta(days=2)
     day_before_yesterday_str = day_before_yesterday.strftime('%Y%m%d')
     # datetime format YYYYMMDDHHMMSS
     arxiv_query = f'({subject}) AND ' \
